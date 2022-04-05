1a73e34e650aefb35690c5ef693a72848603de3b
wanderer-gray <79612007262@yandex.ru>
2022-04-06 01:44:04 +0500

    tmp

--- frontend/src/classes/Api.js
+++ frontend/src/classes/Api.js
@@ -1,23 +1,111 @@
-class Api {
-  constructor() {
-    this.method = 'get';
-    this.url = '';
-    this.params = null;
-    this.query = null;
-    this.body = null;
+export class ApiError extends Error {
+  constructor (status, message) {
+    super(message)
+    this.name = this.constructor.name
+    this.status = status
+  }
+}
+
+export class Api {
+  constructor (url = null) {
+    this._url = url;
+    this._method = null;
+    this._params = null;
+    this._query = null;
+    this._body = null;
   }
 
-  get _url() {
-    return this.url
+  url (url) {
+    this._url = url
+
+    return this
   }
 
-  get _options() {
-    return this.method
+  method (method) {
+    this._method = method
+
+    return this
   }
 
-  async _request() {
-    const response = await fetch(this._url, this._options);
+  params (params) {
+    this._params = params
 
-    
+    return this
+  }
+
+  query (query) {
+    this._query = query
+
+    return this
+  }
+
+  body (body) {
+    this._body = body
+
+    return this
+  }
+
+  _getUrl () {
+    const path = this._url.replace(/(:\w+)/g, (rParam) => {
+      const param = rParam.slice(1);
+
+      if (!this._params || !(param in this._params)) {
+        throw new Error(`Param #${param} not found in ${this._url}`);
+      }
+  
+      return this._params[param];
+    });
+
+    const url = new URL(path);
+
+    if (this._query) {
+      for (const [qKey, qVal] of Object.entries(this._query)) {
+        url.searchParams.set(qKey, qVal);
+      }
+    }
+
+    return url;
+  }
+
+  _getOptions () {
+    const options = {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      method: this._method
+    };
+
+    const body = this._body;
+
+    if (body) {
+      options.body = JSON.stringify(body)
+    }
+
+    return options;
+  }
+
+  _request () {
+    const url = this._getUrl();
+    const options = this._getOptions();
+
+    return fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new ApiError(response.status, response.statusText)
+        }
+
+        return response.json();
+      });
+  }
+  
+  then (resolve, reject) {
+    return this._request()
+      .then(resolve)
+      .catch(reject);
+  }
+
+  catch (reject) {
+    return this._request()
+      .catch(reject);
   }
 }
--- frontend/src/components/lineChart/LineChart.js
+++ frontend/src/components/lineChart/LineChart.js
@@ -10,7 +10,6 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import faker from 'faker';
 
 ChartJS.register(
   CategoryScale,
@@ -43,13 +42,13 @@ export default function LineChart(props) {
     datasets: [
       {
         label: 'Dataset 1',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        data: labels.map(() => Math.random() * 1000),
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
       {
         label: 'Dataset 2',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        data: labels.map(() => Math.random() * 1000),
         borderColor: 'rgb(53, 162, 235)',
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
